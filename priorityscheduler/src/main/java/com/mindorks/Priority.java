7968b1cbc3832c9c6a32fa10b0a89fbdadc15c3e
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2018-07-01 13:57:13 +0530

    Add Priority Enum

---
+++ priorityscheduler/src/main/java/com/mindorks/Priority.java
@@ -0,0 +1,25 @@
+package com.mindorks;
+
+public enum Priority {
+
+    /**
+     * Lowest priority level.
+     */
+    LOW,
+
+    /**
+     * Medium priority level.
+     */
+    MEDIUM,
+
+    /**
+     * Highest priority level.
+     */
+    HIGH,
+
+    /**
+     * Highest priority level.
+     */
+    IMMEDIATE
+
+}
